2e3acc089fa20fe65fd498cc90389099fc9431e6
Eziefule Judith <juditheziefule1@gmail.com>
2024-11-27 13:26:54 -0800

    added a new foldeer

---
+++ Day 20/script.js
@@ -0,0 +1,14 @@
+const profileItem={
+ identity:"Judith",
+ age: "20",
+ school:"Codeflexng",
+   course: "Frontend Dev.",
+   stage: "Beginner",
+   exp: "8month"
+}
+document.getElementById("name").textContent= profileItem.identity;
+document.getElementById("age").textContent= profileItem.age ;
+document.getElementById("university").textContent= profileItem.school ;
+document.getElementById("course").textContent= profileItem.course ;
+document.getElementById("level").textContent= profileItem.stage ;
+document.getElementById("year").textContent= profileItem.exp;
