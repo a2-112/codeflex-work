85938bd2b55f1578bee595e0fc9bbf1fd43977dc
Eziefulejudith <123456789+judith@users.noreply.github.com>
2025-09-02 04:56:52 -0700

    update

--- Day 21/script.js
+++ Day 21/script.js
@@ -1,11 +1,19 @@
 
 const review= document.getElementById("insert");
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
 
 function appendToDisplay(input){
     review.value += input;
+    history();
 }
 function clearDisplay(){
     review.value = "";
+    history();
+}
+function deleteDisplay(){
+    review.value = review.value.slice(0, -1);
 }
 function calculate(){
     try{
@@ -15,3 +23,19 @@ function calculate(){
     review.value = "Error"
    }
 }
+
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-mode');
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+
+    // Save theme preference
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
---
+++ Day 21/style.md
@@ -0,0 +1,29 @@
+
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+> 💡 These are just the design sizes. Ensure content is responsive and meets WCAG requirements by testing the full range of screen sizes from 320px to large screens.
+
+
+## Colors
+
+- Bg: hsl(240, 27%, 18%)
+- Calender Text: hsl(232, 17%, 35%)
+- To do Text: hsl(309, 6%, 58%)
+- Heading: hsl(11, 24%, 72%)
+- Hastag: hsl(21, 35%, 92%)
+
+## Typography
+
+### Fonts
+
+- Headline Font: [Reddit Sans](https://fonts.google.com/specimen/RedditSans)
+
+- Paragraph Font: [Sora](https://fonts.google.com/specimen/Sora)
+
